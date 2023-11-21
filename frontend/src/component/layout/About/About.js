82ff5945e6567bd2e8312b3b840e812ab4e228c1
Hasnain Abbas <hasnain72.qureshi@gmail.com>
2023-11-21 21:52:13 +0500

    done

---
+++ frontend/src/component/layout/About/About.js
@@ -0,0 +1,53 @@
+import React from "react";
+import "./aboutSection.css";
+import { Button, Typography, Avatar } from "@material-ui/core";
+import YouTubeIcon from "@material-ui/icons/YouTube";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import Hasnain from "./image.jpg";
+const About = () => {
+  const visitInstagram = () => {
+    window.location = "https://www.instagram.com/hasnain_abbas72/";
+  };
+  return (
+    <div className="aboutSection">
+      <div></div>
+      <div className="aboutSectionGradient"></div>
+      <div className="aboutSectionContainer">
+        <Typography component="h1">About Us</Typography>
+
+        <div>
+          <div>
+            <Avatar
+              style={{ width: "10vmax", height: "10vmax", margin: "2vmax 0" }}
+              src={Hasnain}
+              alt="Founder"
+            />
+            <Typography>Hasnain Abbas</Typography>
+            <Button onClick={visitInstagram} color="primary">
+              Visit Instagram
+            </Button>
+            <span>
+              This is a sample wesbite made by @HasnainAbbas. Only with the
+              purpose to learn MERN Stack development.
+            </span>
+          </div>
+          <div className="aboutSectionContainer2">
+            <Typography component="h2">Our Brands</Typography>
+            <a
+              href="https://www.youtube.com/"
+              target="blank"
+            >
+              <YouTubeIcon className="youtubeSvgIcon" />
+            </a>
+
+            <a href="https://www.instagram.com/" target="blank">
+              <InstagramIcon className="instagramSvgIcon" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
